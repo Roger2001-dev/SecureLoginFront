1a19a738f4fe489fbc70fa146e188fbaa61f0a36
Rogelio Murillö <rogelio.murillo99@hotmail.com>
2025-09-26 23:05:22 -0600

    Change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44349' 
+  apiUrl: 'https://secureloginapipg20250926222914-dtckghczb3e7a5bw.eastus-01.azurewebsites.net' 
 };
